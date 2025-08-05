502adad1f1c0f2e6cc7c50d704376ac37b39a91d
mohamedtarek963 <mandotareka@gmail.com>
2025-08-05 22:25:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let img =document.querySelector('.img');
+let containar =document.querySelector('.containar');
+function phone(phone){
+    img.src = phone;
+}
+function color(color){
+containar.style.background =color
+}
