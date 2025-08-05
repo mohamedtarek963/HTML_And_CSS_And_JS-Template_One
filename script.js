2eae2d0de528f866f77bdb190631b4a622129132
mohamedtarek963 <mandotareka@gmail.com>
2025-08-05 22:21:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let img =document.querySelector('.img');
-let containar =document.querySelector('.containar');
-function phone(phone){
-    img.src = phone;
-}
-function color(color){
-containar.style.background =color
-}
